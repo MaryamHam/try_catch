74137df7855e5d150a7d5d716e92ae70da6b9420
MaryamHam <107949861+MaryamHam@users.noreply.github.com>
2023-09-02 01:34:06 +0400

    Exception handling

---
+++ bin/app.dart
@@ -0,0 +1,18 @@
+void main() {
+
+try{
+int number1 = 30;
+int number2 = 0;
+int result = number1 ~/number2;
+
+print("Result of first number divided on second number is $result ");
+
+}
+catch(e){
+  print("Exception occurs");
+}
+finally{
+  print("finally block execute");
+}
+
+}
